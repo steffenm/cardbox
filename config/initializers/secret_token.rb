2e8fe5528ba97c9bdd7613a5b3b72c32f516320c
Steffen Moelter <stmo@Steffen-Moelters-MacBook-Pro.local>
2010-11-13 11:03:08 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Karteikarten::Application.config.secret_token = 'e09a176e10703b27163fe8ea0b4bd2c6622e2697423902b57eba841e5be187ea0411a4fcac5662a771bc7fd18ffb1e4a885b264bd2e1241643c065207c98d026'
