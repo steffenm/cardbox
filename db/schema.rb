2ba845a73ec5f5194287c93a5a1b9f780cbe0679
Steffen Moelter <stmo@Steffen-Moelters-MacBook-Pro.local>
2010-11-17 20:23:07 +0100

    delete index from users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101114222633) do
+ActiveRecord::Schema.define(:version => 20101117192005) do
 
   create_table "cards", :force => true do |t|
     t.string   "side1"
